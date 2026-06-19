997ddc4d60c5e8b047450eaead077a2d78572b82
Varun Patil <varunpatil102030@gmail.com>
2026-06-19 20:43:40 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,13 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(
                 new InputStreamReader(System.in));
+
+        Set<String> builtins = Set.of("echo", "exit", "type");
 
         while (true) {
             System.out.print("$ ");
@@ -18,11 +21,13 @@ public class Main {
 
             String[] parts = input.split(" ");
 
-            if (parts[0].equals("exit")) {
+            String command = parts[0];
+
+            if (command.equals("exit")) {
                 break;
             }
 
-            if (parts[0].equals("echo")) {
+            if (command.equals("echo")) {
                 StringBuilder sb = new StringBuilder();
 
                 for (int i = 1; i < parts.length; i++) {
@@ -36,7 +41,19 @@ public class Main {
                 continue;
             }
 
-            System.out.println(parts[0] + ": command not found");
+            if (command.equals("type")) {
+                String target = parts[1];
+
+                if (builtins.contains(target)) {
+                    System.out.println(target + " is a shell builtin");
+                } else {
+                    System.out.println(target + ": not found");
+                }
+
+                continue;
+            }
+
+            System.out.println(command + ": command not found");
         }
     }
 }
